5b9a38cc4596b3a9478d5efaa238d6fcf7f7831f
Craig Landis <clandis@microsoft.com>
2018-03-16 20:08:35 -0700

    test

--- windows/Enable-RDP.ps1
+++
@@ -1,7 +0,0 @@
-﻿write-Host "Enabling RDP";
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name 'fDenyTSConnections' 0 -Type Dword -ErrorAction SilentlyContinue;
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'fDenyTSConnections' 0 -Type Dword -ErrorAction SilentlyContinue;
-Get-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name 'fDenyTSConnections' -ErrorAction SilentlyContinue;
-Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'fDenyTSConnections' -ErrorAction SilentlyContinue;
-Restart-Service -Name TermService -Force;
-Write-Host "Completed Enabling RDP";
--- windows/Get-InstanceMetadata.ps1
+++ windows/Get-InstanceMetadata.ps1
@@ -10,4 +10,4 @@
     See also https://docs.microsoft.com/en-us/azure/virtual-machines/windows/instance-metadata-service
 #>
 
-Invoke-RestMethod -Method GET -Uri http://169.254.169.254/metadata/instance?api-version=2017-04-02 -Headers @{"Metadata"="True"} | ConvertTo-JSON -Depth 99
+invoke-restmethod -Method GET -Uri http://169.254.169.254/metadata/instance?api-version=2017-04-02 -Headers @{'Metadata'='True'} | ConvertTo-Json -Depth 99
