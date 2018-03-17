2d29cde96b37f32af53a733d5adaebf375564d29
Craig Landis <clandis@microsoft.com>
2018-03-16 19:49:09 -0700

    test

--- windows/Fix-RDPPort.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-Host "Repairing RDP listener";
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\control\Terminal Server\Winstations\RDP-Tcp' -name 'PortNumber' 3389 -Type Dword;
-Restart-Service -Name TermService -Force;
-Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\control\Terminal Server\Winstations\RDP-Tcp' -name 'PortNumber';
-Write-Host "Completed repairing RDP listener";
